9e6bdfecfaa18e28b724da44b6bcdef3a1319615
Tristan Gläs <tristan.glaes@gmx.de>
2017-12-03 17:22:12 +0100

    ToDoListe

--- ToDoList/app/src/main/java/com/example/tristanglaes/todolist/DetailsActivity.java
+++ ToDoList/app/src/main/java/com/example/tristanglaes/todolist/DetailsActivity.java
@@ -5,34 +5,65 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 public class DetailsActivity extends AppCompatActivity {
 
-    public static final String intentResultKey = "ResultKey";
-    private Button applyBtn, cancelBtn;
-    private TextView toDoTv, priorityTv;
-    private String oldToDoString;
+    public final static String TITLE_RESULT_KEY = "TITLERESULTKEY";
+    public final static String PRIORITY_RESULT_KEY = "PRIORITYRESULTKEY";
+    public final static String DESCRIPTION_RESULT_KEY = "DESCRIPTIONRESULTKEY";
+    private Button applyBtn, cancelBtn, deleteBtn;
+    private EditText toDoEt, descriptionEt;
+    private Spinner prioSp;
+    private String title, description, oldTitle, oldDescription;
+    private Priority prio, oldPrio;
+    private int spinnerPosition;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_details);
 
-        toDoTv = findViewById(R.id.tvToDoString);
-
+        // View Elemente fuer Title, Priority und Beschreibung
+        toDoEt = findViewById(R.id.tvToDoString);
+        descriptionEt = findViewById(R.id.etDescription);
+        prioSp = findViewById(R.id.toDoSpinner);
 
         Intent intent = getIntent();
-        oldToDoString = intent.getStringExtra(MainActivity.intentKey);
-        toDoTv.setText(oldToDoString);
+        // Auslesen der uebergebenen Daten
+        title = intent.getStringExtra(MainActivity.TITLE_KEY);
+        description = intent.getStringExtra(MainActivity.DESCRIPTION_KEY);
+        prio = (Priority) intent.getSerializableExtra(MainActivity.PRIORITY_KEY);
+
+        oldTitle = title;
+        oldDescription = description;
+        oldPrio = prio;
+
+        toDoEt.setText(title);
+        descriptionEt.setText(description);
+
+        switch (prio){
+            case low: spinnerPosition = 2;
+            break;
+            case middle: spinnerPosition = 1;
+            break;
+            case high: spinnerPosition = 0;
+            break;
+        }
+
+        prioSp.setSelection(spinnerPosition);
 
         applyBtn = findViewById(R.id.applyBtn);
         applyBtn.setOnClickListener(new View.OnClickListener(){
             public void onClick(View v) {
                 Intent result = new Intent();
-                result.putExtra(intentResultKey, toDoTv.getText());
-                /* Rückgabewert und -daten setzen */
-                setResult(RESULT_OK,result);
+                result.putExtra(TITLE_RESULT_KEY, toDoEt.getText().toString());
+                result.putExtra(PRIORITY_RESULT_KEY, Priority.makeEnumFromString(prioSp.getSelectedItem().toString()));
+                result.putExtra(DESCRIPTION_RESULT_KEY, descriptionEt.getText().toString());
+                setResult(RESULT_OK, result);
                 finish();
             }
         });
@@ -41,14 +72,22 @@ public class DetailsActivity extends AppCompatActivity {
         cancelBtn.setOnClickListener(new View.OnClickListener(){
             public void onClick(View v) {
                 Intent result = new Intent();
-                result.putExtra(intentResultKey, oldToDoString);
-                /* Rückgabewert und -daten setzen */
-                setResult(RESULT_OK,result);
+                result.putExtra(TITLE_RESULT_KEY, oldTitle);
+                result.putExtra(PRIORITY_RESULT_KEY, oldPrio);
+                result.putExtra(DESCRIPTION_RESULT_KEY, oldDescription);
+                setResult(RESULT_OK, result);
                 finish();
             }
         });
 
-
-
+        deleteBtn = findViewById(R.id.deleteBtn);
+        deleteBtn.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v) {
+                Intent result = new Intent();
+                result.putExtra(TITLE_RESULT_KEY, "");
+                setResult(RESULT_OK, result);
+                finish();
+            }
+        });
     }
 }
--- ToDoList/app/src/main/java/com/example/tristanglaes/todolist/MainActivity.java
+++ ToDoList/app/src/main/java/com/example/tristanglaes/todolist/MainActivity.java
@@ -14,52 +14,91 @@ import android.widget.AdapterView.OnItemClickListener;
 
 public class MainActivity extends AppCompatActivity {
 
-    private final static int REQUEST_CODE = 1;
-    public final static String intentKey = "ToDoString";
+    private final static int REQUEST_CODE_DETAILS = 1;
+    private final static int REQUEST_CODE_NEW_TO_DO = 2;
+    public final static String TITLE_KEY = "TITLEKEY";
+    public final static String PRIORITY_KEY = "PRIORITYKEY";
+    public final static String DESCRIPTION_KEY = "DESCRIPTIONKEY";
+
     public static int listViewItemIndex = 0;
-    private static final String[] toDoList = new String[]{
-            "ANdroid uebung"
-    };
-    private ArrayAdapter<String> adapter = null;
+    /*private static final String[] toDoList = new String[]{
+            "Android Uebung machen", "Rechnerarchitektur"
+    };*/
+    private ArrayAdapter<ToDoEntry> adapter = null;
     private ListView toDoListView;
+    private MenuItem newItem;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
-        adapter = new ArrayAdapter<>(
+        /*/adapter = new ArrayAdapter<>(
                 this, android.R.layout.simple_list_item_1); // des Adapters
         for (int i = 0; i < toDoList.length; i++) {
             adapter.add(toDoList[i]);
-        }
+        }*/
+        final MainActivity ma = this;
+
+        adapter = new ArrayAdapter<ToDoEntry>(this, android.R.layout.simple_list_item_1);
+        adapter.add(new ToDoEntry("Tristan ist der Beste", Priority.low, "Jo isser"));
+        adapter.add(new ToDoEntry("Jurie ist der Beste", Priority.high, "Ist er nicht"));
+        adapter.add(new ToDoEntry("Meris ist klasse!", Priority.middle, "Test"));
+
+        adapter.sort(new ToDoEntry("Tristan ist der Beste", Priority.high, "Jo isser"));
 
         toDoListView = findViewById(R.id.toDoList);
         toDoListView.setAdapter(adapter);
-        final MainActivity ma = this;
+
+
         toDoListView.setOnItemClickListener(new OnItemClickListener(){
             @Override
             public void onItemClick(AdapterView av, View v, int i, long l){
                 Intent intent = new Intent(ma, DetailsActivity.class);
-                intent.putExtra(intentKey, adapter.getItem(i));
+                intent.putExtra(TITLE_KEY, adapter.getItem(i).getTitle());
+                intent.putExtra(PRIORITY_KEY, adapter.getItem(i).getPriority());
+                intent.putExtra(DESCRIPTION_KEY, adapter.getItem(i).getDescription());
                 listViewItemIndex = i;
                 /* Starten der Activity */
-                startActivityForResult(intent, REQUEST_CODE);
-
+                startActivityForResult(intent, REQUEST_CODE_DETAILS);
             }
         });
-
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (requestCode == REQUEST_CODE) {
-            String result = data.getStringExtra(DetailsActivity.intentResultKey);
-            adapter.remove(adapter.getItem(listViewItemIndex));
-            adapter.insert(result, listViewItemIndex);
+        if (requestCode == REQUEST_CODE_DETAILS) {
+
+            String newTitle = data.getStringExtra(DetailsActivity.TITLE_RESULT_KEY);
+            Priority newPrio = (Priority) data.getSerializableExtra(DetailsActivity.PRIORITY_RESULT_KEY);
+            String newDescription = data.getStringExtra(DetailsActivity.DESCRIPTION_RESULT_KEY);
+
+            if(newTitle.equals("")){
+                adapter.remove(adapter.getItem(listViewItemIndex));
+            } else {
+                ToDoEntry tDE = adapter.getItem(listViewItemIndex);
+                tDE.setTitle(newTitle);
+                tDE.setDescription(newDescription);
+                tDE.setPriority(newPrio);
+                adapter.sort(tDE);
+            }
+        } else if(requestCode == REQUEST_CODE_NEW_TO_DO){
+
+            if(resultCode == RESULT_OK) {
+                String newTitle = data.getStringExtra(NewActivity.NEW_TITLE_RESULT_KEY);
+                Priority newPrio = (Priority) data.getSerializableExtra(NewActivity.NEW_PRIORITY_RESULT_KEY);
+                String newDescription = data.getStringExtra(NewActivity.NEW_DESCRIPTION_RESULT_KEY);
+
+                ToDoEntry tDE = new ToDoEntry(newTitle, newPrio, newDescription);
+                adapter.add(tDE);
+                adapter.sort(tDE);
+            } else if(resultCode == RESULT_CANCELED){
+                return;
+            }
+
         }
     }
 
@@ -86,5 +125,10 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void newDoTo(MenuItem item) {
+        Intent intent = new Intent(this, NewActivity.class);
+        startActivityForResult(intent, REQUEST_CODE_NEW_TO_DO);
     }
 }
